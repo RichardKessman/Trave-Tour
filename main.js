461ff7f7de3bcb373bd04004b07180b62b0cbb2a
Richard Kessman <121726877+RichardKessman@users.noreply.github.com>
2023-05-10 01:20:39 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function () {
+    header.classList.toggle ("sticky", this.window.scrollY > 0)
+});
+
+
+let menu = document.querySelector('#menu-icon');
+
+let navBar = document.querySelector('.navBar');
+
+menu.onclick = () => {
+    menu.classList.toggle ('bx-x');
+    navBar.classList.toggle ('open');
+};
+
+
+window.onscroll = () => {
+    menu.classList.remove ('bx-x');
+    navBar.classList.remove ('open');
+};
